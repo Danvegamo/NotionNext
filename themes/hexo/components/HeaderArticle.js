8b9f4a651eb8194efd2c151a0f6f33e709c3cff5
David Vega <102634786+Danvegamo@users.noreply.github.com>
2023-01-05 17:32:33 +0000

    restored size of header, and up size of letter

--- themes/hexo/components/HeaderArticle.js
+++ themes/hexo/components/HeaderArticle.js
@@ -22,10 +22,10 @@ export default function HeaderArticle({ post, siteInfo }) {
       className="w-full h-96 relative md:flex-shrink-0 overflow-hidden bg-cover bg-center bg-no-repeat animate__animated animate__fadeIn"
       style={{ backgroundImage: headerImage }}
     >
-      <header className="animate__slideInDown animate__animated bg-black bg-opacity-70 absolute top-0 w-full h-96 py-20 flex justify-center items-center ">
+      <header className="animate__slideInDown animate__animated bg-black bg-opacity-70 absolute top-0 w-full h-96 py-10 flex justify-center items-center ">
         <div className='mt-24'>
           {/* Article Title */}
-          <div className="font-bold text-xl shadow-text flex justify-center text-center text-white dark:text-white ">
+          <div className="font-bold text-5xl shadow-text flex justify-center text-center text-white dark:text-white ">
             {post.title}
           </div>
 
