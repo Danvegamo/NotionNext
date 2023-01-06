a3ab0434ef01075fd6bc01ff57aa91b1d29dd46d
David Vega <102634786+Danvegamo@users.noreply.github.com>
2023-01-06 00:54:51 +0000

    update copy

--- themes/hexo/components/ArticleCopyright.js
+++ themes/hexo/components/ArticleCopyright.js
@@ -33,7 +33,7 @@ export default function ArticleCopyright () {
         </li>
         <li>
           <strong className='mr-2'>{locale.COMMON.COPYRIGHT}:</strong>
-          {All_rights_reserved}
+          {locale.COMMON.COPYRIGHT_NOTICE}
         </li>
       </ul>
     </section>
